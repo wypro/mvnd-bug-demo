7cffce8c45ed349de5f8bb1a1f39bd4232d0a5c1
wangyong <wangyong@caohua.com>
2022-01-21 18:17:19 +0800

    git@github.com:wypro/mvnd-bug-demo.git

---
+++ src/main/java/com/mvnd/Main.java
@@ -0,0 +1,14 @@
+package com.mvnd;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import java.util.logging.Logger;
+
+public class Main {
+        final static Log log = LogFactory.getLog(Main.class);
+
+        public static void main(String[] args) {
+                log.info("test run...");
+        }
+}
